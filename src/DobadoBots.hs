79fdf63053fa4600c460d8926dc0a9f6bd7e7b77
Baylac-Jacqué Félix <felix@alternativebit.fr>
2017-05-10 16:03:22 +0200

    Implemented basic datatypes.

--- src/DobadoBots.hs
+++ src/DobadoBots.hs
@@ -1,7 +1,9 @@
 module DobadoBots(
   parseScript
 , module DobadoBots.Interpreter.Data
+, module DobadoBots.GameEngine.Data
 ) where
 
 import DobadoBots.Interpreter.Data
+import DobadoBots.GameEngine.Data
 import DobadoBots.Interpreter.Interpreter (parseScript)
---
+++ src/DobadoBots/GameEngine/Data.hs
@@ -0,0 +1,29 @@
+module DobadoBots.GameEngine.Data (
+  Size(..)
+, GameEngine(..)
+, Object(..)
+, Obstacle(..)
+, Objective(..)
+, StartingPoint(..)
+) where
+import Linear.V2
+
+type Size = (Float, Float)
+
+data GameEngine = GameEngine {obstacles ::[Obstacle] 
+                            , objective :: Objective
+                            , startingPoints :: [StartingPoint]
+                            , robots :: [Robot]
+
+data Object = Object {position :: V2 Float
+                    , size     :: Size
+                    , rotation :: Float
+                    , velocity :: V2 Float} deriving (Show)
+
+type Obstacle = Object
+
+type Objective = Object
+
+type StartingPoint = Object
+
+type Robot = Object 
