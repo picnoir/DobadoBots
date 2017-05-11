0b0fe45438a053d7950e4387ec3fa97e2eadf05e
Baylac-Jacqué Félix <felix@alternativebit.fr>
2017-05-11 19:16:46 +0200

    Implemented rectangles generator.

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,15 @@
 
 import Control.Monad (unless)
 import Data.Text (Text(..))
-import DobadoBots (createMainWindow, closeMainWindow, mainGraphicsLoop)
+import DobadoBots (createMainWindow, closeMainWindow, mainGraphicsLoop, GameEngine(..))
+
+-- TODO write new event loop.
+engineState :: GameEngine
+engineState = undefined
 
 main :: IO ()
 main = do
   (renderer, window) <- createMainWindow "DobadoBots" 
-  mainGraphicsLoop renderer 
+  mainGraphicsLoop renderer engineState
   closeMainWindow renderer window
 
--- src/DobadoBots/Graphics/Window.hs
+++ src/DobadoBots/Graphics/Window.hs
@@ -5,10 +5,13 @@ module DobadoBots.Graphics.Window (
 ) where
 import Control.Monad (unless)
 import Data.Text (Text(..))
+import qualified Data.Vector.Storable as V (Vector(..), fromList, map) 
 import qualified SDL
 import SDL (($=))
 import SDL.Vect (V2(..), V4(..))
-import Foreign.C.Types (CInt)
+import qualified Linear.V2 as L (V2(..))
+import Foreign.C.Types (CInt(..))
+import DobadoBots.GameEngine.Data (GameEngine(..), Objective(..), Obstacle(..), Object(..))
 
 screenWidth, screenHeight :: CInt
 (screenWidth, screenHeight) = (640, 480)
@@ -35,12 +38,37 @@ closeMainWindow renderer window = do
   SDL.destroyWindow window
   SDL.quit
 
-mainGraphicsLoop :: SDL.Renderer -> IO ()
-mainGraphicsLoop renderer = do 
-  events <- SDL.pollEvents
-  let quit = elem SDL.QuitEvent $ map SDL.eventPayload events
+handleEvents :: IO ([SDL.Event])
+handleEvents = SDL.pollEvents
+
+mainGraphicsLoop :: SDL.Renderer -> GameEngine -> IO ()
+mainGraphicsLoop renderer gameState = do 
+  --let quit = elem SDL.QuitEvent $ map SDL.eventPayload events
   SDL.rendererDrawColor renderer $= V4 maxBound maxBound maxBound maxBound
+  drawArena renderer gameState 
   SDL.clear renderer
   SDL.present renderer
-  unless quit $ mainGraphicsLoop renderer
 
+getObstaclesRects :: [Obstacle] -> V.Vector (SDL.Rectangle CInt)
+getObstaclesRects obs = V.fromList $ rectangles 
+  where
+    rectangles   = map rectangle obs
+    rectangle ob = SDL.Rectangle (pointRect ob) (sizeRect ob)
+    pointRect ob = SDL.P $ linearToSDLV2 $ position ob
+    sizeRect ob  = linearToSDLV2 $ size ob
+
+getObjectiveRect :: Objective -> SDL.Rectangle CInt
+getObjectiveRect obj  = SDL.Rectangle (pointRect obj) (sizeRect obj)
+  where pointRect obj = SDL.P $ linearToSDLV2 $ position obj
+        sizeRect obj  = linearToSDLV2 $ size obj
+  
+
+drawArena :: SDL.Renderer -> GameEngine -> IO ()
+drawArena renderer gameState = do
+  let obsR = getObstaclesRects $ obstacles gameState
+  let objR = getObjectiveRect $ objective gameState
+  SDL.drawRects renderer obsR
+
+linearToSDLV2 :: L.V2 Float -> SDL.V2 CInt
+linearToSDLV2 (L.V2 x y) = SDL.V2 (toCint x) (toCint y)  
+  where toCint x = CInt $ floor x
