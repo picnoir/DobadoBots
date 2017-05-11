b09e88861c3c1859dd82c5dd4d42d10955659c0f
Baylac-Jacqué Félix <felix@alternativebit.fr>
2017-05-11 19:59:43 +0200

    Refactored application mainloop.

--- app/Main.hs
+++ app/Main.hs
@@ -2,15 +2,24 @@
 
 import Control.Monad (unless)
 import Data.Text (Text(..))
-import DobadoBots (createMainWindow, closeMainWindow, mainGraphicsLoop, GameEngine(..))
-
--- TODO write new event loop.
-engineState :: GameEngine
-engineState = undefined
+import qualified Data.Text.IO as TIO (readFile)
+import DobadoBots (createMainWindow, closeMainWindow, mainGraphicsLoop, GameEngine(..), loadLevel)
+import qualified SDL (EventPayload(..), eventPayload, pollEvents, Renderer)
 
 main :: IO ()
 main = do
+  levelStr <- TIO.readFile "data/levels/level1.json"
+  engineState <- getState $ loadLevel levelStr
   (renderer, window) <- createMainWindow "DobadoBots" 
-  mainGraphicsLoop renderer engineState
+  mainLoop renderer engineState
   closeMainWindow renderer window
+  where
+    getState (Right state) = return state
+    getState (Left err) = fail err
 
+mainLoop :: SDL.Renderer -> GameEngine -> IO ()
+mainLoop r st = do
+  evts <- SDL.pollEvents
+  mainGraphicsLoop r st
+  let quit = elem SDL.QuitEvent $ map SDL.eventPayload evts
+  unless quit $ mainLoop r st
