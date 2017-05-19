97f70f19abeb8557b53f25337345d21939f846c1
Félix Baylac-Jacqué <felix@alternativebit.fr>
2017-05-19 20:42:29 +0200

    Writing up to date tests.

--- test/DobadoBots/GameEngineSpec.hs
+++ test/DobadoBots/GameEngineSpec.hs
@@ -2,7 +2,7 @@
 
 module DobadoBots.GameEngineSpec where
 
-import DobadoBots (loadLevel, Object(..), GameEngine(..))
+import DobadoBots (loadLevel, Object(..), Level(..), Size(..))
 
 import Test.Hspec (describe, it, Spec(..), shouldBe)
 import Data.Text.Encoding (decodeUtf8)
@@ -14,10 +14,10 @@ spec :: Spec
 spec = describe "parseArena" $ do
         it "should parse simple JSON" $ do
           loadLevel (decodeUtf8 $(embedFile "test/fixtures/levels/level1.json")) `shouldBe` Right(
-            GameEngine 
+           Level
               [(Object (V2 12.3 13.2) (V2 10.0 12.0) 0 0 )]
+              (V2 640 480)
               (Object (V2 50 50) (V2 10 12) 0.0 0 )
               [(Object (V2 0 0) (V2 10 12) 0 0 )]
-              (S.fromList [(Object (V2 0 0) (V2 10 12) 0 0 )])
             )
             
