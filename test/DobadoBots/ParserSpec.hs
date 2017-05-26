0882945bd6e5e9734c5eac980245f1addf4dd2ae
Félix Baylac-Jacqué <felix@alternativebit.fr>
2017-05-26 13:32:58 +0200

    Updated parser unit tests.

--- test/DobadoBots/ParserSpec.hs
+++ test/DobadoBots/ParserSpec.hs
@@ -25,21 +25,21 @@ spec = describe "parseScript" $ do
          it "should parse simple cond =" $ 
            parseScript (decodeUtf8 $(embedFile "test/fixtures/script/simplecond.script")) `shouldBe` Right (Cond (CmpLogicInt $ Eq LaserDistance 1)(Token TurnLeft) (Token MoveForward))
          it "should parse simple cond >" $
-           parseScript (decodeUtf8 $(embedFile "test/fixtures/script/simplecond2.script")) `shouldBe` Right (Cond (CmpLogicInt $ Sup LaserScan 1)(Token TurnLeft) (Token MoveForward))
+           parseScript (decodeUtf8 $(embedFile "test/fixtures/script/simplecond2.script")) `shouldBe` Right (Cond (CmpLogicInt $ Inf LaserScan 1)(Token TurnLeft) (Token MoveForward))
          it "should parse simple cond <" $ 
-           parseScript (decodeUtf8 $(embedFile "test/fixtures/script/simplecond3.script")) `shouldBe` Right (Cond (CmpLogicInt $ Inf LaserDistance 1)(Token TurnLeft) (Token MoveForward))
+           parseScript (decodeUtf8 $(embedFile "test/fixtures/script/simplecond3.script")) `shouldBe` Right (Cond (CmpLogicInt $ Sup LaserDistance 1)(Token TurnLeft) (Token MoveForward))
          it "should parse collider " $
            parseScript (decodeUtf8 $(embedFile "test/fixtures/script/simplecondcol.script")) `shouldBe` Right (Cond (CmpCollider LaserScan Wall) (Token TurnLeft) (Token MoveForward))
          it "should parse weirdly indented files" $ 
-           parseScript (decodeUtf8 $(embedFile "test/fixtures/script/simpleBadIndented.script")) `shouldBe` Right (Cond (CmpLogicInt $ Inf LaserDistance 1)(Token TurnLeft) (Token MoveForward))
+           parseScript (decodeUtf8 $(embedFile "test/fixtures/script/simpleBadIndented.script")) `shouldBe` Right (Cond (CmpLogicInt $ Sup LaserDistance 1)(Token TurnLeft) (Token MoveForward))
          it "should parse tab indented files" $ 
-           parseScript (decodeUtf8 $(embedFile "test/fixtures/script/simpleTabIndented.script")) `shouldBe` Right (Cond (CmpLogicInt $ Inf LaserDistance 1)(Token TurnLeft) (Token MoveForward))
+           parseScript (decodeUtf8 $(embedFile "test/fixtures/script/simpleTabIndented.script")) `shouldBe` Right (Cond (CmpLogicInt $ Sup LaserDistance 1)(Token TurnLeft) (Token MoveForward))
          it "should parse nested if cond" $ 
-           parseScript (decodeUtf8 $(embedFile "test/fixtures/script/nestedcond.script")) `shouldBe` Right (Cond (CmpLogicInt $ Sup LaserDistance 10) (Cond (CmpLogicInt $ Sup LaserDistance 20) (Token TurnLeft) (Token TurnRight)) (Token MoveForward))
+           parseScript (decodeUtf8 $(embedFile "test/fixtures/script/nestedcond.script")) `shouldBe` Right (Cond (CmpLogicInt $ Inf LaserDistance 10) (Cond (CmpLogicInt $ Inf LaserDistance 20) (Token TurnLeft) (Token TurnRight)) (Token MoveForward))
          it "should parse nested else cond" $ 
-           parseScript (decodeUtf8 $(embedFile "test/fixtures/script/nestedcond2.script")) `shouldBe` Right (Cond (CmpLogicInt $ Sup LaserDistance 10)  (Token TurnLeft) (Cond (CmpLogicInt $ Sup LaserDistance 20) (Token TurnLeft) (Token TurnRight)))
+           parseScript (decodeUtf8 $(embedFile "test/fixtures/script/nestedcond2.script")) `shouldBe` Right (Cond (CmpLogicInt $ Inf LaserDistance 10)  (Token TurnLeft) (Cond (CmpLogicInt $ Inf LaserDistance 20) (Token TurnLeft) (Token TurnRight)))
          it "should parse deeply nested else cond" $ 
-           parseScript (decodeUtf8 $(embedFile "test/fixtures/script/deepNestedCond.script")) `shouldBe` Right (Cond (CmpLogicInt $ Sup LaserDistance 10)  (Token TurnLeft) (Cond (CmpLogicInt $ Sup LaserDistance 20) (Cond (CmpLogicInt $ Inf ObjectiveDistance 3) (Token MoveForward) (Token TurnLeft)) (Token TurnRight)))
+           parseScript (decodeUtf8 $(embedFile "test/fixtures/script/deepNestedCond.script")) `shouldBe` Right (Cond (CmpLogicInt $ Inf LaserDistance 10)  (Token TurnLeft) (Cond (CmpLogicInt $ Inf LaserDistance 20) (Cond (CmpLogicInt $ Sup ObjectiveDistance 3) (Token MoveForward) (Token TurnLeft)) (Token TurnRight)))
          it "should fail when no operator in condition"$ 
            parseScript (decodeUtf8 $(embedFile "test/fixtures/script/simpleconderr1.script")) `shouldSatisfy` isLeft 
          it "should fail when no success action"$ 
