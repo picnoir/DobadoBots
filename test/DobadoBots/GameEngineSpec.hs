360b90916a2f2da5782f980c31acc7a5dff87f0d
Baylac-Jacqué Félix <felix@alternativebit.fr>
2017-06-19 18:24:05 +0200

    Fixed unit tests after velocity refactor.

--- test/DobadoBots/GameEngineSpec.hs
+++ test/DobadoBots/GameEngineSpec.hs
@@ -15,9 +15,9 @@ spec = describe "parseArena" $
         it "should parse simple JSON" $ 
           loadLevel (decodeUtf8 $(embedFile "test/fixtures/levels/level1.json")) `shouldBe` Right(
            Level
-              [Object (V2 12.3 13.2) (V2 10.0 12.0) 0 0 ]
+              [Object (V2 12.3 13.2) (V2 10.0 12.0) 0 0 0 ]
               (V2 640 480)
-              (Object (V2 50 50) (V2 10 12) 0.0 0 )
-              [Object (V2 0 0) (V2 10 12) 0 0 ]
+              (Object (V2 50 50) (V2 10 12) 0 0 0 )
+              [Object (V2 0 0) (V2 10 12) 0 0 0 ]
             )
             
